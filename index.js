945a62de0d097bf41147b5752fe3b3cdad4db3de
Leonardo Bobadilla <135734651+leoelpokemo@users.noreply.github.com>
2025-11-26 13:43:36 -0600

    cooooors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./db.js";
 import { Card } from "./models/Card.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectDB();
 
 //Create
