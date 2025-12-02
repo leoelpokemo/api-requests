5f3d5013b4a77431d75725f892088e82a925ce4e
Leonardo Bobadilla <135734651+leoelpokemo@users.noreply.github.com>
2025-12-02 12:06:52 -0600

    like

--- models/Card.js
+++ models/Card.js
@@ -5,6 +5,10 @@ const cardSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     link: { type: String, required: true, trim: true },
     description: { type: String, required: true },
+    like: {
+      type: Boolean,
+      default: false,
+    },
   },
   { timestamps: true }
 );
